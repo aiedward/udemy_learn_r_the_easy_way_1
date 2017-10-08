cc8ba2ef278dc9cb19493d40ed4516957608859d
yaojenkuo <tonykuoyj@gmail.com>
2017-10-08 13:33:06 +0800

    first commit

---
+++ named_list.R
@@ -0,0 +1,10 @@
+# Creating a list
+genre <- "Sitcom"
+seasons <- 10
+starring <- data.frame(
+  stars = c("Jennifer Aniston", "Courteney Cox", "Lisa Kudrow", "Matt LeBlanc",
+            "Matthew Perry", "David Schwimmer"),
+  characters = c("Rachel Green", "Monica Geller", "Phoebe Buffay",
+                 "Joey Tribbiaani", "Chandler Bing", "Ross Geller")
+)
+friends_list <- list(Genre = genre, Seasons = seasons, Starring = starring)
